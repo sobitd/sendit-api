0f96cb9191349b91187fb3e0117a9438a4ae354b
sobitd <sobitdaniel@gmail.com>
2022-10-31 08:49:52 +0300

    add routes and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
@@ -22,16 +23,8 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
   end
 
   # PATCH/PUT /users/1 or /users/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
   resources :parcels
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  delete 'logout', to: 'sessions#destroy'
+  get 'authorized', to: 'sessions#page_requires_login'
   # Defines the root path route ("/")
   # root "articles#index"
 end
