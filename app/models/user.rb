bc320fea3f34b6015547ee442c7d5118c7e17807
sobitd <sobitdaniel@gmail.com>
2022-10-31 09:03:39 +0300

    add dependent destroy for user-parcel association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_many :parcels
-    validates :first_name, :last_name,:email_address,:phone_number,:username,:password_digest, presence: true 
+  has_secure_password
+  has_many :parcels, dependent: :destroy
+  validates :first_name, :last_name, :email_address, :phone_number, :username, :password_digest, presence: true
 end
